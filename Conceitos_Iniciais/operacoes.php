c7441f02814ddb162a3462a5b25e9a91d3d9d7d4
Gabriel Miranda <gabrielmirandazacarias@gmail.com>
2023-10-31 18:46:28 -0300

    Avançando nos fundamentos

---
+++ Conceitos_Iniciais/operacoes.php
@@ -0,0 +1,17 @@
+<?php
+
+$idade = 21;
+
+$idadeDaqui10Anos = $idade + 10;
+
+$soma = 2 + 2;
+$subtracao = 2 - 2;
+$multiplicacao = 2 * 2;
+$divisao = 2 / 2;
+$doisAoCubo = 2 ** 2;
+$restoDaDivisao = 3 % 2;
+
+echo $subtracao;
+echo $multiplicacao;
+echo $restoDaDivisao;
+echo $doisAoCubo;
---
+++ Conceitos_Iniciais/tipos.php
@@ -0,0 +1,13 @@
+<?php
+
+$inteiro = 24;
+$salario = 2000.45;
+$texto = "Molezinha";
+$_vivo = true;
+$_morto = false;
+
+echo gettype($inteiro);
+echo gettype($salario);
+echo gettype($texto);
+echo gettype($_vivo);
+echo gettype($_morto);
